2b2a237f6d84b669b1555d20cd068540e6ac535e
Tosby <o2jose43@gmail.com>
2017-05-26 19:36:32 +0300

    Completed the registration interface design

--- app/src/main/java/com/otema/staffattendancemanager/Login.java
+++ app/src/main/java/com/otema/staffattendancemanager/Login.java
@@ -1,13 +1,27 @@
 package com.otema.staffattendancemanager;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Login extends AppCompatActivity {
-
+Button btnSignUp, btnLogin;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        // Get the reference of the buttons
+        btnLogin = (Button)findViewById(R.id.sign_in_button);
+        btnSignUp = (Button) findViewById(R.id.sign_up_button);
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                /// Create Intent for SignUpActivity  abd Start The Activity
+                Intent intentSignUP=new Intent(getApplicationContext(),Register.class);
+                startActivity(intentSignUP);
+            }
+        });
+
     }
 }
