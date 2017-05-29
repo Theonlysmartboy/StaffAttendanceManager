648f91ecf24d9527e58456505453b53e88f9f6b6
Tosby <o2jose43@gmail.com>
2017-05-29 12:20:49 +0300

    Begun the signup logics

--- app/src/main/java/com/otema/staffattendancemanager/Login.java
+++ app/src/main/java/com/otema/staffattendancemanager/Login.java
@@ -5,23 +5,38 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
-public class Login extends AppCompatActivity {
-Button btnSignUp, btnLogin;
+public class Login extends AppCompatActivity implements View.OnClickListener {
+    private Button btnSignUp, btnLogin;
+    private EditText uname, pwd;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        // Get the reference of the buttons
+
+        // Get the reference of the buttons and text fields
+
         btnLogin = (Button)findViewById(R.id.sign_in_button);
         btnSignUp = (Button) findViewById(R.id.sign_up_button);
-        btnSignUp.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                /// Create Intent for SignUpActivity  abd Start The Activity
-                Intent intentSignUP=new Intent(getApplicationContext(),Register.class);
-                startActivity(intentSignUP);
-            }
-        });
+        uname = (EditText) findViewById(R.id.username);
+        pwd = (EditText) findViewById(R.id.password);
+        btnSignUp.setOnClickListener(this);
+        btnLogin.setOnClickListener(this);
+    }
 
+    @Override
+    public void onClick(View view) {
+switch (view.getId()){
+    case R.id.sign_in_button:
+
+        break;
+    case R.id.sign_up_button:
+        Intent sign_up = new Intent(getApplicationContext(),Register.class);
+        startActivity(sign_up);
+        break;
+    default:
+
+}
     }
 }
--- app/src/main/java/com/otema/staffattendancemanager/Register.java
+++ app/src/main/java/com/otema/staffattendancemanager/Register.java
@@ -2,12 +2,26 @@ package com.otema.staffattendancemanager;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class Register extends AppCompatActivity {
-
+public class Register extends AppCompatActivity implements View.OnClickListener{
+    private Button btnsignup;
+    private EditText uname,pwd,rpwd;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        btnsignup = (Button) findViewById(R.id.buttonCreateAccount);
+        uname = (EditText) findViewById(R.id.editTextUserName);
+        pwd = (EditText) findViewById(R.id.editTextPassword);
+        rpwd = (EditText) findViewById(R.id.editTextConfirmPassword);
+        btnsignup.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+
     }
 }
